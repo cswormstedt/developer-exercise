28d514604aea08ed1418e335e103c58cb971e7dc
Naomi Mine <naomi.mine@gmail.com>
2017-06-10 21:35:22 -0500

    accessed data, figuring out how to render it

--- javascript/simple_backbone/quotes.js
+++ javascript/simple_backbone/quotes.js
@@ -10,50 +10,88 @@ var QuoteModel = Backbone.Model.extend({
 });
 
 var QuotesCollection = Backbone.Collection.extend({
-  // url: 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json',
-  model: QuoteModel
-  // parse: function(data) {
-  //   return data.books;
-  // }
+  	url: 'https://gist.githubusercontent.com/anonymous/8f61a8733ed7fa41c4ea/raw/1e90fd2741bb6310582e3822f59927eb535f6c73/quotes.json',
+  	model: QuoteModel,
+ 	parse: function(data) {
+	    return data;
+	}
 });
 
 var Quotes = new QuotesCollection;
 
-var quote1 = new QuoteModel({
-  quote: 'hello,'
-  context: 'here',
-  source: 'now',
-  theme: 'forever'
-});
 
-Quotes.add(quote1);
 
-Quotes.each(function(quote) {
-   console.log(quote.get('quote'))
-  $('#table-body').append(
-	'<tr>' +
-	  '<td>' + quote.get('quote') + '</td>' +
-	  '<td>' + quote.get('context') + '</td>' +
-	  '<td>' + quote.get('source') + '</td>' +
-	  '<td>' + quote.get('theme') + '</td>' +
-	'</tr>'
-  );
-});
+InputView = Backbone.View.extend({
+	el: '#search-div',
+	initialize: function(){
+		this.render();
+	},
+	template: 
+		"<h2>Search quotes</h2> \
+		<input type='text' placeholder='search'> \
+        <button>Search movie</button> \
+        <ul id='quote-list'></ul>",
+    render: function() {
+        this.$el.html(this.template);
+        return this;
+    }
 
-QuoteView = Backbone.View.extend({
+})
+
+
+QuotesView = Backbone.View.extend({
 	el: '#table-body',
 	initialize: function(){
-
+		this.renderQuotes();
 	},
-	render: function(){
-
+	randFunct: function(){
+		console.log("calling rand")
+		var quotes = new QuotesCollection({model: QuoteModel});
+		var self = this;
+		quotes.fetch({success: function(res){
+			console.log(res)
+			}
+		});
+	},
+	renderQuotes: function(quotes){
+		console.log(quotes)
+		this.randFunct();
+		this.$el.html('');
+		new InputView;
+		Quotes.each(function(model) {
+			var quote = new QuoteView({
+				model: model
+			})
+			this.$el.append(quote.render().el);
+		}.bind(this));
+		return this;
 	}
 
 });
 
 
 
+QuoteView = Backbone.View.extend({
+	tagName: 'tr',
+	template: _.template($('#quote-template').html()),
+	// renderQuotes: function(quotes){
+	// 	var quotes = []
+	// 	for(i=0;i<quotes.length;i++){
 
+	// 	}
+ //    },
+	render: function() {
+	    this.$el.html(this.template(this.model.attributes));
+	    return this;
+	}
+})
 
+var app = new QuotesView;
+
+var QuoteApp = {
+    Views: {},
+    Models: {},
+    Collections: {}
+}
 
 
